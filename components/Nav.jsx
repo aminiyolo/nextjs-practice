194c3f229c66287a63bc97183805d0f0ba18b1bf
aminiyolo <aminiyolo@gmail.com>
2023-10-18 23:49:09 +0900

    feat: Home page 기초 작업

---
+++ components/Nav.jsx
@@ -0,0 +1,134 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+
+const Nav = () => {
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
+  const [isToggleDropdown, setIsToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+
+    setProvider();
+  }, []);
+
+  // alert(session?.user);
+
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>logo_text</p>
+      </Link>
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {session?.user ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='sm:hidden flex relative'>
+        {session?.user ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => setIsToggleDropdown((prev) => !prev)}
+            />
+            {isToggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setIsToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setIsToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  className='mt-5 w-full black_btn'
+                  onClick={() => {
+                    setIsToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
