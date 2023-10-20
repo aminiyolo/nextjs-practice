d9f806dfa193c59e895dab099d21306e0ab60688
aminiyolo <aminiyolo@gmail.com>
2023-10-20 15:54:13 +0900

    Prompt 저장을 위한 route, schema 파일 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ['mongoose'],
+    serverComponentsExternalPackages: ['mongoose'], // 해당 옵션을 사용하면, Next.js의 서버 사이드 렌더링 기능을 사용할 때, 서버 측에서 필요한 외부 패키지를 지정하고 번들링하여 사용하게 된다.
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
